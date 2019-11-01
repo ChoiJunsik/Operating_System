a5a62199ce256fa8497ee1eeed62784b1daf1935
ChoiJunsik <2318ws@naver.com>
2019-11-01 16:28:49 +0900

    add:default console

--- 02.Kernel64/Source/Main.c
+++ 02.Kernel64/Source/Main.c
@@ -2,6 +2,8 @@
 #include "Keyboard.h"
 #include "Descriptor.h"
 #include "PIC.h"
+#include "Console.h"
+#include "ConsoleShell.h"
 // 함수 선언
 void kPrintString( int iX, int iY, const char* pcString );
 BOOL canRead(DWORD* addr);
@@ -11,81 +13,63 @@ BOOL canWrite(DWORD* addr);
  */
 void Main( void )
 {
-    char vcTemp[ 2 ] = { 0, };
-    BYTE bFlags;
-    BYTE bTemp;
-    int i = 0;
-    KEYDATA stData;
+    int iCursorX, iCursorY;
 
-    kPrintString( 0, 12, "Switch To IA-32e Mode Success~!!" );
-    kPrintString( 0, 13, "IA-32e C Language Kernel Start..............[Pass]" );
-    kPrintString( 0, 14, "This message is printed through the video memory relocated to 0xAB8000" );
-    if(canRead((DWORD*)0x1fe000)){
-        kPrintString( 0, 15, "Read from 0x1fe000 [Ok]" );
-    }else kPrintString( 0, 15, "Read from 0x1fe000 [No]" );
-    if(canWrite((DWORD*)0x1fe000)){
-        kPrintString( 0, 16, "Write to 0x1fe000 [Ok]" );
-    }else kPrintString( 0, 16, "Write to 0x1fe000 [No]" );
-    if(canRead((DWORD*)0x1FF000)){
-        kPrintString( 0, 17, "Read from 0x1ff000 [Ok]" );
-    }else kPrintString( 0, 17, "Read from 0x1ff000 [No]" );
-    // if(canWrite((DWORD*)0x1FF000)){
-    //     kPrintString( 0, 18, "Write to 0x1ff000 [Ok]" );
-    // }else kPrintString( 0, 18, "Write to 0x1ff000 [No]" );
-    kPrintString( 0, 19, "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+    // 콘솔을 먼저 초기화한 후, 다음 작업을 수행
+    kInitializeConsole( 0, 10 );    
+    kPrintf( "Switch To IA-32e Mode Success~!!\n" );
+    kPrintf( "IA-32e C Language Kernel Start..............[Pass]\n" );
+    kPrintf( "Initialize Console..........................[Pass]\n" );
+    
+    // 부팅 상황을 화면에 출력
+    kGetCursor( &iCursorX, &iCursorY );
+    kPrintf( "GDT Initialize And Switch For IA-32e Mode...[    ]" );
     kInitializeGDTTableAndTSS();
     kLoadGDTR( GDTR_STARTADDRESS );
-    kPrintString( 45, 19, "Pass" );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
     
-    kPrintString( 0, 20, "TSS Segment Load............................[    ]" );
+    kPrintf( "TSS Segment Load............................[    ]" );
     kLoadTR( GDT_TSSSEGMENT );
-    kPrintString( 45, 20, "Pass" );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
     
-    kPrintString( 0, 21, "IDT Initialize..............................[    ]" );
+    kPrintf( "IDT Initialize..............................[    ]" );
     kInitializeIDTTables();    
     kLoadIDTR( IDTR_STARTADDRESS );
-    kPrintString( 45, 21, "Pass" );
-    kPrintString( 0, 22, "Keyboard Activate And Queue Initialize......[    ]" );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
+    
+    kPrintf( "Total RAM Size Check........................[    ]" );
+    kCheckTotalRAMSize();
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass], Size = %d MB\n", kGetTotalRAMSize() );
+    
+    kPrintf( "Keyboard Activate And Queue Initialize......[    ]" );
     // 키보드를 활성화
     if( kInitializeKeyboard() == TRUE )
     {
-        kPrintString( 45, 22, "Pass" );
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Pass\n" );
         kChangeKeyboardLED( FALSE, FALSE, FALSE );
     }
     else
     {
-        kPrintString( 45, 22, "Fail" );
+        kSetCursor( 45, iCursorY++ );
+        kPrintf( "Fail\n" );
         while( 1 ) ;
     }
-    kPrintString( 0, 23, "PIC Controller And Interrupt Initialize.....[    ]" );
+    
+    kPrintf( "PIC Controller And Interrupt Initialize.....[    ]" );
     // PIC 컨트롤러 초기화 및 모든 인터럽트 활성화
     kInitializePIC();
     kMaskPICInterrupt( 0 );
     kEnableInterrupt();
-    kPrintString( 45, 23, "Pass" );    
-    while( 1 )
-    {
-        // 키 큐에 데이터가 있으면 키를 처리함
-        if( kGetKeyFromKeyQueue( &stData ) == TRUE )
-        {
-            // 키가 눌러졌으면 키의 ASCII 코드 값을 화면에 출력
-            if( stData.bFlags & KEY_FLAGS_DOWN )
-            {
-                // 키 데이터의 ACII 코드 값을 저장
-                vcTemp[ 0 ] = stData.bASCIICode;
-                kPrintString( i++, 24, vcTemp );
+    kSetCursor( 45, iCursorY++ );
+    kPrintf( "Pass\n" );
 
-                // 0이 입력되면 변수를 0으로 나누어 Divide Error 예외(벡터 0번)을
-                // 발생시킴
-                if( vcTemp[ 0 ] == '0' )
-                {
-                    // 아래 코드를 수행하면 Divide Error 예외가 발생하여
-                    // 커널의 임시 핸들러가 수행됨
-                    bTemp = bTemp / 0;
-                }
-            }
-        }
-    }
+    // 셸을 시작
+    kStartConsoleShell();
 }
 BOOL canRead(DWORD* addr){
     DWORD val = 0x1994;
